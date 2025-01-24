3a5b8bdc06036912c2d8b7b2ce5038bafe9a2ae5
ronak <ronakpraja340@gmail.com>
2025-01-24 19:03:50 +0530

    defined signup and sigin logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,73 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL : string
+    JWT_SECRET : string
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-  // return c.text('Hello Hono!')
+app.post("/api/v1/signup",async (c)=>{
+  
+  const prisma = new PrismaClient({
+    datasourceUrl : c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json()
+  try {
+    const user = await prisma.user.create({
+      data:{
+        email : body.email,
+        password : body.password,
+        name : body.name
+      }
+    })
+    
+    const token = await sign({id : user},c.env.JWT_SECRET)
+    
+    return c.json({jwt : token})
+    
+  } catch (error) {
+    c.status(403)
+    return c.json({message: "User already exists/error while creating user"})
+  }
 })
 
-app.post("/api/v1/signup",(c)=>{
-  return c.json({message: "signup"})
+
+app.post("/api/v1/signin",async (c)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl : c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json()
+  const user = await prisma.user.findFirst({
+    where: {
+      email: body.email
+    }
+  })
+  if(!user){
+    c.status(403)
+    return c.json({error : "user not found"})
+  }
+
+  const jwt = sign({id : user},c.env.JWT_SECRET)
+  return c.json({jwt})
 })
-app.post("/api/v1/signin",(c)=>{
-  return c.json({message: "signin"})
-})
+
+
 app.post("/api/v1/blog",(c)=>{
   return c.json({message: "Blogs post"})
 })
+
+
 app.put("/api/v1/blog",(c)=>{
   return c.json({message: "Blogs put"})
 })
+
+
 app.get("/api/v1/blog/:id",(c)=>{
   return c.json({message: "Blogs get"})
 })
