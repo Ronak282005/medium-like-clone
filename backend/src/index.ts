4c9ced6f15ec0ce7c28c5f5f249c7b2d38f3fd11
ronak <ronakpraja340@gmail.com>
2025-01-25 18:11:30 +0530

    defined blog route logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,41 +17,7 @@ app.route("/api/v1/user",userRouter)
 app.route("/api/v1/blog",blogRouter)
 
 
-// auth middleware
-app.use("/api/v1/blog/*",async (c,next)=>{
-  const authorization = c.req.header('authorization')
-  if(!authorization){
-    c.status(403)
-    return c.json({message: "Unauthorized"})
-  }
-  const token = authorization.split(" ")[1]
-  const jwt = await verify(token,c.env.JWT_SECRET)
-  if(jwt.id){
-    next()
-  }else{
-    c.status(403)
-    return c.json({message: "Unauthorized"})
-  }
-})
 
 
-
-
-// blog routes post
-app.post("/api/v1/blog",(c)=>{
-  return c.json({message: "Blogs post"})
-})
-
-
-// blog routes put
-app.put("/api/v1/blog",(c)=>{
-  return c.json({message: "Blogs put"})
-})
-
-
-// blog routes get
-app.get("/api/v1/blog/:id",(c)=>{
-  return c.json({message: "Blogs get"})
-})
 
 export default app
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,3 +1,52 @@
 import { Hono } from "hono";
+import { sign, verify } from 'hono/jwt'
 
-export const blogRouter = new Hono();
+
+export const blogRouter = new Hono<{
+    Bindings: {
+      DATABASE_URL : string
+      JWT_SECRET : string
+    }
+  }>();
+
+
+// auth middleware
+blogRouter.use("/*",async (c,next)=>{
+    const authorization = c.req.header('authorization')
+    if(!authorization){
+      c.status(403)
+      return c.json({message: "Unauthorized"})
+    }
+    const token = authorization.split(" ")[1]
+    const jwt = await verify(token,c.env.JWT_SECRET)
+    if(jwt.id){
+      next()
+    }else{
+      c.status(403)
+      return c.json({message: "Unauthorized"})
+    }
+  })
+
+
+// blog routes post
+blogRouter.post("/",async(c)=>{
+    return c.json({message: "Blogs post"})
+})
+  
+  
+  // blog routes put
+blogRouter.put("/",async(c)=>{
+    return c.json({message: "Blogs put"})
+})
+  
+  
+// blog routes get
+blogRouter.get("/",async(c)=>{
+    return c.json({message: "Blogs get"})
+})
+
+
+blogRouter.get("/bulk",async(c)=>{
+    return c.json({message: "Blogs get"})
+})
+
