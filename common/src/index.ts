832c45ba73b5303c158a585111f5b1b8cc76ca58
ronak <ronakpraja340@gmail.com>
2025-01-25 22:31:52 +0530

    published the zod validation to the npm

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,12 +1,11 @@
 import { z } from "zod";
 
+
 export const signupInput = z.object({
     email : z.string().email(),
     password : z.string().min(6),
     name : z.string().optional()
 })
-
-export type SignupInput = z.infer<typeof signupInput> 
 
 
 export const signinInput = z.object({
@@ -14,14 +13,12 @@ export const signinInput = z.object({
     password : z.string().min(6),
 })
 
-export type SigninInput = z.infer<typeof signinInput>
 
 export const createBlogInput = z.object({
     title : z.string(),
     content : z.string(),
 })
 
-export type CreateBlogInput = z.infer<typeof createBlogInput> 
 
 export const updateBlogInput = z.object({
     title : z.string().optional(),
@@ -29,4 +26,8 @@ export const updateBlogInput = z.object({
     id : z.string()
 })
 
+
+export type SignupInput = z.infer<typeof signupInput> 
+export type SigninInput = z.infer<typeof signinInput>
+export type CreateBlogInput = z.infer<typeof createBlogInput> 
 export type UpdateBlogInput = z.infer<typeof updateBlogInput>
