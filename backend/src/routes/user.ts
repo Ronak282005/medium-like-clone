421878d32ca5864efab81d5a3b553f674e367cfc
ronak <ronakpraja340@gmail.com>
2025-01-25 22:36:50 +0530

    added zod validation

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign, verify } from 'hono/jwt'
-import { signupInput } from "../zod";
+import { signupInput } from "@ronak3333/medium-common"
 
 export const userRouter = new Hono<{
     Bindings: {
