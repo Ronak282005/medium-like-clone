732d81302f5352405e461c8189cb413df95f6639
ronak <ronakpraja340@gmail.com>
2025-01-26 00:23:20 +0530

    frontend routes init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+import { Blogs } from './pages/Blogs'
 
 function App() {
   return (
     <>
-    <div>
-      hi there
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/signin" element={<Signin/>} />
+      <Route path="/blog/:id" element={<Blogs/>} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
