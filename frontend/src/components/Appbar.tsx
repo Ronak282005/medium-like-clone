da3066250b60f78dc653c73d89c3a96d5a82533c
ronak <ronakpraja340@gmail.com>
2025-01-27 13:31:46 +0530

    Added appbar component

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -3,10 +3,10 @@ import { Avatar } from "./BlogCard"
 export const Appbar = () => {
   return (
     <div className="border-b flex justify-between items-center px-10 py-4">
-        <div>
+        <div className="flex flex-col justify-center">
             Medium
         </div>
-        <Avatar name="ronak"/>
+        <Avatar name="ronak" size="big"/>
     </div>
   )
 }
