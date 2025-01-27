8cf5cf1403f76ff665c4401428b9a007c9e1e73c
ronak <ronakpraja340@gmail.com>
2025-01-27 13:25:11 +0530

    modified the code

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -2,7 +2,11 @@ import { BlogCard } from "../components/BlogCard"
 
 export const Blogs = () => {
   return (
-    <BlogCard />
+    <div className="flex justify-center">
+        <div className="max-w-xl">
+            <BlogCard />
+        </div>
+    </div>
   )
 }
 
