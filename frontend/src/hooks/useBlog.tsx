9e248a55cef046d082540558788b85fc4f0b93cc
ronak <ronakpraja340@gmail.com>
2025-01-29 00:00:22 +0530

    added blog interface

--- frontend/src/hooks/useBlog.tsx
+++ frontend/src/hooks/useBlog.tsx
@@ -2,9 +2,19 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { BACKEND_URL } from "../config"
 
+
+interface Blog {
+    id: number
+    title: string
+    content: string
+    author: string
+    publishedDate: string
+    name : string
+}
+
 export const useBlog = () => {
     const [loding, setLoading] = useState(true)
-    const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState<Blog[]>([])
 
     useEffect(()=>{
         axios.get(`${BACKEND_URL}/api/v1/blog/bulk`, {
