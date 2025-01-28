03351681e015901bbc9730b6541154d9ccb66477
ronak <ronakpraja340@gmail.com>
2025-01-28 23:53:22 +0530

    added useBlog hook in the blogs

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,10 +1,17 @@
 import { BlogCard } from "../components/BlogCard"
+import { useBlog } from "../hooks/useBlog"
 
 export const Blogs = () => {
+  const {loding, blogs} = useBlog()
+  if(loding){
+      return <div className="flex justify-center">Loading...</div>
+  }
   return (
     <div className="flex justify-center">
         <div className="max-w-xl">
+          {blogs.map(blog =>{
             <BlogCard />
+          })}
         </div>
     </div>
   )
