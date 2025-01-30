e8093dc839b251d99caa5e9ca9e3aa99e5ea0b4b
ronak <ronakpraja340@gmail.com>
2025-01-31 00:48:26 +0530

    id in jwt

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -33,7 +33,7 @@ userRouter.post("/signup",async (c)=>{
         }
       })
       
-      const token = await sign({id : user},c.env.JWT_SECRET)
+      const token = await sign({id : user.id},c.env.JWT_SECRET)
 
       return c.json({jwt : token})
       
@@ -65,6 +65,6 @@ userRouter.post("/signup",async (c)=>{
       return c.json({error : "user not found"})
     }
     
-    const jwt = await sign({id : user},c.env.JWT_SECRET)
+    const jwt = await sign({id : user.id},c.env.JWT_SECRET)
     return c.json({jwt})
   })
