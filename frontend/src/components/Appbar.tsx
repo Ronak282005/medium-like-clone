7331db32a88c63ff539a6909cca679452562c898
ronak <ronakpraja340@gmail.com>
2025-01-31 22:38:15 +0530

    added Link

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
 
 export const Appbar = () => {
   return (
     <div className="border-b flex justify-between items-center px-10 py-4">
-        <div className="flex flex-col justify-center">
-            Medium
-        </div>
+        <Link to={"/"}>
+          <div className="flex flex-col justify-center">
+              Medium
+          </div>
+        </Link>
         <Avatar name="ronak" size="big"/>
     </div>
   )
