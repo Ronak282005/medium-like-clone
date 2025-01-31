6dc0a89e0fee0c49f9b290d35b3bc80aad7b3ac3
ronak <ronakpraja340@gmail.com>
2025-01-31 22:36:53 +0530

    added shimmer ui

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,13 +1,18 @@
 import { useParams } from "react-router-dom"
 import { FullBlog } from "../components/FullBlog"
 import { useBlog } from "../hooks"
+import BlogSkeleton from "../components/BlogSkeleton"
+import HeaderSkeleton from "../components/HeaderSkeleton"
 
 export const Blog = () => {
   const {id} = useParams()
   const {loading,blog} = useBlog({
     id : id || ""
   })
-  if(loading) return <div>Loading...</div>
+  if(loading) return <div>
+    <HeaderSkeleton/>
+    <BlogSkeleton />
+  </div>
   return (
     <div>
       {blog ? <FullBlog blog={blog}/> : <div>No blog found</div>}
